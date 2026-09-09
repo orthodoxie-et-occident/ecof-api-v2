d55da7b635aca2ba3bd1db8b3c65aecb1e5ff08d
JacquesRaoult <raoult.jacques@gmail.com>
2026-09-09 15:01:54 +0200

    Add CORS middleware

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { Hono } from "hono"
+import { cors } from "hono/cors"
 import { synaxarRoutes } from "./features/synaxar/synaxar.routes"
 import { mapRoutes } from "./features/map/map.routes"
 import { readingRoutes } from "./features/reading/reading.routes"
@@ -7,6 +8,8 @@ import { parishRoutes } from "./features/parish/parish.routes"
 import { calendarRoutes } from "./features/liturgical-calendar/liturgical-calendar.route"
 
 const app = new Hono()
+
+app.use("*", cors())
 
 app.route("/synaxar", synaxarRoutes)
 app.route("/map", mapRoutes)
