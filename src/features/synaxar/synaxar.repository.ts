3a4facff2cdfb5fe8eeac96367add1e3bea8864b
JacquesRaoult <raoult.jacques@gmail.com>
2026-09-09 09:14:50 +0200

    Edit synaxar repository to match db

--- src/features/synaxar/synaxar.repository.ts
+++ src/features/synaxar/synaxar.repository.ts
@@ -4,7 +4,7 @@ type SynaxarRow = {
   v_short: string | null
   v_long: string | null
   v_liturgy: string | null
-  has_img: boolean
+  has_img: boolean | null
   mois: number
   jour: number
   prefixe: string
@@ -23,9 +23,18 @@ export async function getSynaxar() {
 
 export async function getVita(id: string) {
   const rows = await db<SynaxarRow[]>`
-        SELECT v_short, v_long, v_liturgy, has_img, mois, jour, prefixe, saint
-        FROM synaxar
-        WHERE vies_id = ${id}
+        SELECT 
+            v.v_short, 
+            v.v_long, 
+            v.v_liturgy, 
+            v.has_img, 
+            s.mois, 
+            s.jour, 
+            s.prefixe, 
+            s.saint
+        FROM synaxar s
+        LEFT JOIN vita v ON v.vies_id = s.vies_id
+        WHERE s.vies_id = ${id}
         LIMIT 1
     `
   return rows || null
